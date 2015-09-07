1fbb465c8061de7d1cf486f113d704ee2d841331
xXAwesomeXx <zainjrajani@gmail.com>
2015-09-07 12:03:55 -0400

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,7 +5,7 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [blue, green, blue, green, red];
+var letterColors = [blue, green, blue, green, blue, green, blue, greeen, blue, green, blue, green];
 
 drawName(ZQRN, letterColors);
 
